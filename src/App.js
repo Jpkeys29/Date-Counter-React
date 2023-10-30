bca8f7926f0991088fda500aafca12bcccc64e22
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-10-30 11:44:00 -0400

    Counter setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Counter or Testing</h1>
+    </div>
+  );
+}
+
+function Counter() {
+  return (
+    <div>
+      Testing
+    </div>
+  )
+}
