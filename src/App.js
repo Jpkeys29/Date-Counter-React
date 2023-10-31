5c731c2368f63841bae5a21d8083e945cd9b0fd2
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-10-31 07:26:29 -0400

    Setting steps state

--- src/App.js
+++ src/App.js
@@ -11,16 +11,24 @@ export default function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
   const date = new Date("December 18 2023");
   date.setDate(date.getDate() + count);
 
 
   return (
     <div>
-      
-        <button onClick={() => setCount((c) => c - 1)}>-</button>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
         <span>Count: {count}</span>
-        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
       <p> 
         <span>{count === 0 ? 'Today is ' : count > 0 ?
         `${count} days from today is ` : `${Math.abs(count)} days ago was `} </span>
