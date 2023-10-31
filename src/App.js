c4375580a64c26f33160e9f78f817eb580b23f6c
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-10-31 07:17:19 -0400

    Ternary operator

--- src/App.js
+++ src/App.js
@@ -17,12 +17,15 @@ function Counter() {
 
   return (
     <div>
-      {/* <div> */}
+      
         <button onClick={() => setCount((c) => c - 1)}>-</button>
         <span>Count: {count}</span>
         <button onClick={() => setCount((c) => c + 1)}>+</button>
-      {/* </div> */}
-      <p> {date.toDateString()} </p>
+      <p> 
+        <span>{count === 0 ? 'Today is ' : count > 0 ?
+        `${count} days from today is ` : `${Math.abs(count)} days ago was `} </span>
+        <span>{date.toDateString()}</span> 
+      </p>
     </div>
   );
 }
